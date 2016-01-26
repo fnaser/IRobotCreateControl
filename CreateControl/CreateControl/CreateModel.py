47d683212bdd01ad4a72ce6a304c5402e0a63337
Jeffrey Lipton <jeffreyilipton@gmail.com>
2016-01-25 19:59:10 -0500

    changed to a list of numpy arrays

--- CreateControl/CreateControl/CreateModel.py
+++ CreateControl/CreateControl/CreateModel.py
@@ -1,4 +1,5 @@
 from math import *
+import numpy as np
 
 '''
 let the vehicle have two wheels with speeds U1 and U2. lets U1 be the one on the left and U2 the one on the right when looking form above with the front of hte vehicle
@@ -35,7 +36,7 @@ def UkFromXkanXkplusone(Xk,Xkp1,ro,dt):
     V = sqrt(  ((Xkp1[0]-Xk[0])/dt)**2  + ((Xkp1[1]-Xk[1])/dt)**2 )
     thetadot = (Xkp1[2]-Xk[2])/dt
     Uk = [V-ro*thetadot, V+ro*thetadot]
-    return Uk 
+    return np.array(Uk) 
 
 
 def TrajToUko(Xks,ro,dt):
@@ -43,6 +44,6 @@ def TrajToUko(Xks,ro,dt):
     for i in range(0,len(Xks)-1):
         Xk = Xks[i]
         Xkp1 = Xks[i+1]
-        Ukos.append(UkFromXkanXkplusone(Xk,Xkp1,ro,dt))
+        Ukos.append(np.array(UkFromXkanXkplusone(Xk,Xkp1,ro,dt)))
     return Ukos
 
