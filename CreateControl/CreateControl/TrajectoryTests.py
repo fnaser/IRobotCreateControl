ac3d08081386f3f880f6dab2775e0ab12eb51901
Jeffrey Lipton <jeffreyilipton@gmail.com>
2016-03-29 10:47:28 -0400

    added second order states to trajecotry

--- CreateControl/CreateControl/TrajectoryTests.py
+++ CreateControl/CreateControl/TrajectoryTests.py
@@ -12,11 +12,21 @@ def circle(r,dt,speed):
     c = pi*r*2
     n = int(round(c/dist))
     Xks=[]
+    lastXk = []
     for i in range(0,n+1):
         x = r*cos(2*pi/n*i)
         y = r*sin(2*pi/n*i)
         theta = (2*pi/n*i+pi/2.0)%(2*pi)
-        Xks.append([x,y,theta])
+        Xk = [x,y,theta]
+        if i==0: 
+            lastXk = [0,0,0]
+            dXk = [0,0,0]
+        else:
+            dXk = [ (Xk[i]-lastXk[i])/dt for i in range(0,3) ]
+        lastXk = Xk
+        finalXk = Xk+dXk
+
+        Xks.append(finalXk)
 
     return np.array(Xks)
 
@@ -28,12 +38,15 @@ def main():
     xs = circle(r,dt,speed)
     fig,ax=pl.subplots()
     X = np.array(xs)
-    ax.plot(X[:,0],X[:,1],'bx-')
+    #ax.plot(X[:,0],X[:,1],'bx-')
+    ax.plot(X[:,3])
     ax.autoscale()
     ax.margins(0.1)
     ax.axis('equal')
     plt.axis('equal')
     plt.show()
+
+
 
 
 if __name__ == "__main__":
