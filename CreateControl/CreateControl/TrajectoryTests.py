d289caefa8b5842b1ab88d40d60debcf956b4334
Jeffrey Lipton <jeffreyilipton@gmail.com>
2016-05-17 18:11:16 -0400

    cleaned plotting

--- CreateControl/CreateControl/TrajectoryTests.py
+++ CreateControl/CreateControl/TrajectoryTests.py
@@ -44,16 +44,7 @@ def loadTraj(file):
     return np.load(file)
 
 
-def main():
-    r=254.0 # 10 in 254cm 
-    dt=1.0/5.0
-    speed = 26.5988178 #mm / s
-    xs = circle(r,dt,speed)
-
-    xs = straight(100,1.0/5.0,speed)
-
-    xs = loadTraj('../Media/Cardiod-rc300.00-spacing5.00-rcut127.00-trajs-0.npy')
-
+def plotUandX(xs,UKs):
     fig,ax=pl.subplots()
     X = np.array(xs)
 
@@ -62,11 +53,6 @@ def main():
     plt.plot(X[:,0],X[:,1],'bx-')
     plt.axis('equal')
 
-
-    
-
-    r_wheel = 125
-    UKs = TrajToUko(xs,r_wheel,dt)
     U = np.vstack(UKs)
     
     T = np.linspace(0,X.shape[0]*dt,U.shape[0])
@@ -78,9 +64,54 @@ def main():
     T = np.linspace(0,X.shape[0]*dt,X.shape[0])
     plt.plot(T,X[:,0],'go-')
     plt.plot(T,X[:,1],'bx-')
-    #print U
-
     plt.show()
+
+def vectorsFromOPt(qpt):
+    pt = [qpt[0],qpt[1]]
+    T=[]
+    if(len(qpt)>2): 
+        if (qpt[2] !=None):
+            T = [cos(qpt[2]),sin(qpt[2]) ]
+    else: T = None
+    return [pt,T]
+
+def plotTraj(xs):
+    quivers = []
+    for i in range(0,xs.shape[0]):
+        pt, vec = vectorsFromOPt(xs[i])
+        pt = pt+vec
+        quivers.append(pt)
+    q = np.array(quivers)
+    X,Y,U,V = zip(*q)
+
+    fig,ax=pl.subplots()
+    ax.quiver(X,Y,U,V,angles='xy',scale_units='xy',scale=10, color='k')
+    ax.autoscale()
+    ax.margins(0.1)
+    ax.axis('equal')
+    plt.axis('equal')
+    plt.show()
+
+
+
+
+
+def main():
+    r=254.0 # 10 in 254cm 
+    dt=1.0/5.0
+    speed = 26.5988178 #mm / s
+    xs = circle(r,dt,speed)
+
+    xs = straight(100,1.0/5.0,speed)
+
+    xs = loadTraj('../Media/Cardiod-rc300.00-spacing5.00-rcut127.00-trajs-0.npy')
+
+
+    r_wheel = 125
+    UKs = TrajToUko(xs,r_wheel,dt)
+
+    #plotUandX(xs,UKs)
+    plotTraj(xs)
     return 0
 
 
